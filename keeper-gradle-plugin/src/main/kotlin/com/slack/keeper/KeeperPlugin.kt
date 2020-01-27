71c5e7fba6ef32f5a0eab8b3d4d6071eb413db55
Zac Sweers <zsweers@slack-corp.com>
2020-01-27 15:37:36 -0500

    Https everywhere

--- keeper-gradle-plugin/src/main/kotlin/com/slack/keeper/KeeperPlugin.kt
+++ keeper-gradle-plugin/src/main/kotlin/com/slack/keeper/KeeperPlugin.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *    https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -103,7 +103,7 @@ class KeeperPlugin : Plugin<Project> {
 
         // This is the maven repo where r8 releases are hosted
         repositories {
-          maven("http://storage.googleapis.com/r8-releases/raw")
+          maven("https://storage.googleapis.com/r8-releases/raw")
         }
 
         val appExtension = project.extensions.getByType<AppExtension>()
