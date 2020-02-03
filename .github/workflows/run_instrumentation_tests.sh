078c462c6e48b8f9b738bc16ca429e5bcd835acc
Zac Sweers <zsweers@slack-corp.com>
2020-02-03 00:35:13 -0500

    Try tweaking command

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-./gradlew connectedCheck -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=$CI_AGP_VERSION
-./gradlew connectedCheck -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=$CI_AGP_VERSION
+CI_AGP_VERSION=$1
+
+./gradlew connectedCheck -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+./gradlew connectedCheck -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
