c06fc3aad0d6e8fc43e6d5664c6292e0cb5a16e5
Zac Sweers <zsweers@slack-corp.com>
2020-02-03 04:10:50 -0500

    Try switching order

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -2,5 +2,5 @@
 
 CI_AGP_VERSION=$1
 
-./gradlew connectedCheck -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
 ./gradlew connectedCheck -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+./gradlew connectedCheck -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
