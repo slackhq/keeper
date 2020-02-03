998e16be0d146d406f254a8e0eb9abe6bbbba39c
Zac Sweers <zsweers@slack-corp.com>
2020-02-03 15:36:04 -0500

    Dump stacks on timeout

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -2,5 +2,12 @@
 
 CI_AGP_VERSION=$1
 
-./gradlew connectedCheck -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+function printthreads {
+  echo "Thread dump"
+  jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs jstack
+}
+
+trap printthreads SIGINT
+
+timeout --signal=SIGINT 5m ./gradlew connectedCheck -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
 ./gradlew connectedCheck -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
