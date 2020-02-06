28097e131df0e7b634b5c519a214c4943380325d
Zac Sweers <zsweers@slack-corp.com>
2020-02-06 18:20:55 -0500

    Remove timeout

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -2,14 +2,7 @@
 
 CI_AGP_VERSION=$1
 
-function printthreads {
-  echo "Thread dump"
-  jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs jstack
-}
-
-trap printthreads SIGINT
-
 echo "Running with Proguard!"
-timeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
 echo "Running with R8!"
 ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
