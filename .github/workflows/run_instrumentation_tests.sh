417ac7a56e1595c2ac6c30a058331bccbdea554c
Zac Sweers <zsweers@slack-corp.com>
2020-02-06 19:05:10 -0500

    Add timeouts back!

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -2,7 +2,14 @@
 
 CI_AGP_VERSION=$1
 
+function printthreads {
+  echo "Thread dump"
+  jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs jstack
+}
+
+trap printthreads SIGINT
+
 echo "Running with Proguard!"
-./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+timeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
 echo "Running with R8!"
-./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+timeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
