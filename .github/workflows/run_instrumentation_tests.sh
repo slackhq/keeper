1e222a9de76e329e05e5e1169a7ac12da5d755ac
Zac Sweers <pandanomic@gmail.com>
2020-06-08 01:39:28 -0400

    Minot instrumentation tweaks

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -10,10 +10,10 @@ function printthreads {
 
 trap printthreads SIGINT
 
-echo "Install coreutils"
+echo "Install coreutils" # For gtimeout
 brew install coreutils
 
 echo "Running with Proguard!"
-gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
 echo "Running with R8!"
-gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion=${CI_AGP_VERSION}
+gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
