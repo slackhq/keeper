5ec68984e72ffdcf304bcdcfc598d9ff36bbfb50
Zac Sweers <pandanomic@gmail.com>
2020-09-14 17:51:32 -0400

    Try without proguard for now

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -13,7 +13,7 @@ trap printthreads SIGINT
 echo "Install coreutils" # For gtimeout
 brew install coreutils
 
-echo "Running with Proguard!"
-gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
+#echo "Running with Proguard!"
+#gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=false --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
 echo "Running with R8!"
 gtimeout --signal=SIGINT 5m ./gradlew connectedExternalStagingAndroidTest -Pandroid.enableR8=true --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
