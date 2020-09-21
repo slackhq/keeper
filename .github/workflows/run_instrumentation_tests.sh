ae8638aa1005cf317333ee86d54882254896e896
Zac Sweers <pandanomic@gmail.com>
2020-09-21 15:30:20 -0400

    Reclaim memory mid-gradle task

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -14,4 +14,8 @@ echo "Install coreutils" # For gtimeout
 brew install coreutils
 
 # We only run the sample with R8 as proguard infinite loops if we have java 8 libraries on the classpath 🙃
+./gradlew :sample:minifyExternalStagingWithR8 --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
+# Reclaim memory because Actions OOMs sometimes with having both an emulator and heavy gradle builds going on
+./gradlew --stop && jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs kill -9 || true
+# Now proceed, with much of the build being cached up to this point
 gtimeout --signal=SIGINT 10m ./gradlew connectedExternalStagingAndroidTest --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
