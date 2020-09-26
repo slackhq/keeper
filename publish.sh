8678424548e1a129dc7f66a28e24aaccace97e32
Zac Sweers <pandanomic@gmail.com>
2020-09-25 20:05:25 -0400

    Prepare next development version.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 if [[ "$1" = "--snapshot" ]]; then snapshot=true; fi
 if [[ "$1" = "--local" ]]; then local=true; fi
 
-cd keeper-gradle-plugin
+cd keeper-gradle-plugin || exit
 if ! [[ ${local} ]]; then
   ./gradlew clean uploadArchives --no-daemon --no-parallel -Pkeeper.releaseMode=true
   if ! [[ ${snapshot} ]]; then
