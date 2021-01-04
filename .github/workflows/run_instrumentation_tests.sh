7b905ccf8064ff30e35943def2c2f1657fdaf0e3
Zac Sweers <pandanomic@gmail.com>
2021-01-04 15:36:50 -0500

    Run tracerefs on sample in CI

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CI_AGP_VERSION=$1
+AGP_VERSION=$1
+ENABLE_TRACEREFS=$1
 
 function printthreads {
   echo "Thread dump"
@@ -14,8 +15,8 @@ echo "Install coreutils" # For gtimeout
 brew install coreutils
 
 # We only run the sample with R8 as proguard infinite loops if we have java 8 libraries on the classpath 🙃
-./gradlew :sample:minifyExternalStagingWithR8 --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
+./gradlew :sample:minifyExternalStagingWithR8 --stacktrace -PkeeperTest.agpVersion="${AGP_VERSION}" -PkeeperTest.enableTraceReferences="${ENABLE_TRACEREFS}"
 # Reclaim memory because Actions OOMs sometimes with having both an emulator and heavy gradle builds going on
 ./gradlew --stop && jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs kill -9 || true
 # Now proceed, with much of the build being cached up to this point
-gtimeout --signal=SIGINT 10m ./gradlew connectedExternalStagingAndroidTest --stacktrace -PkeeperTest.agpVersion="${CI_AGP_VERSION}"
+gtimeout --signal=SIGINT 10m ./gradlew connectedExternalStagingAndroidTest --stacktrace -PkeeperTest.agpVersion="${AGP_VERSION}" -PkeeperTest.enableTraceReferences="${ENABLE_TRACEREFS}"
