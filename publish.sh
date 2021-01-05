2e3efa01fd9914045cd7e2c853f74599908618a7
Zac Sweers <pandanomic@gmail.com>
2021-01-04 19:14:54 -0500

    Use -p in publish

--- publish.sh
+++ publish.sh
@@ -3,14 +3,11 @@
 if [[ "$1" = "--snapshot" ]]; then snapshot=true; fi
 if [[ "$1" = "--local" ]]; then local=true; fi
 
-cd keeper-gradle-plugin || exit
 if ! [[ ${local} ]]; then
-  ./gradlew clean uploadArchives --no-daemon --no-parallel -Pkeeper.releaseMode=true
+  ./gradlew -p keeper-gradle-plugin clean uploadArchives --no-daemon --no-parallel -Pkeeper.releaseMode=true
   if ! [[ ${snapshot} ]]; then
-    ./gradlew closeAndReleaseRepository
+    ./gradlew -p keeper-gradle-plugin closeAndReleaseRepository
   fi
 else
-  ./gradlew clean install --no-daemon --no-parallel -Pkeeper.releaseMode=true
+  ./gradlew -p keeper-gradle-plugin clean install --no-daemon --no-parallel -Pkeeper.releaseMode=true
 fi
-
-cd ..
