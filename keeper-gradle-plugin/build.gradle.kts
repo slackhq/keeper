490c77f3d3202fe19a0b40ba30be40fce7da2aff
Zac Sweers <zsweers@slack-corp.com>
2021-02-25 12:51:02 -0500

    Update dependencies  (#75)

--- keeper-gradle-plugin/build.gradle.kts
+++ keeper-gradle-plugin/build.gradle.kts
@@ -20,8 +20,8 @@ import java.net.URL
 plugins {
   `kotlin-dsl`
   `java-gradle-plugin`
-  kotlin("jvm") version "1.4.21"
-  kotlin("kapt") version "1.4.21"
+  kotlin("jvm") version "1.4.30"
+  kotlin("kapt") version "1.4.30"
   id("org.jetbrains.dokka") version "1.4.20"
   id("com.vanniktech.maven.publish") version "0.13.0"
 }
@@ -113,8 +113,8 @@ val agpVersion = findProperty("keeperTest.agpVersion")?.toString() ?: defaultAgp
 // See https://github.com/slackhq/keeper/pull/11#issuecomment-579544375 for context
 val releaseMode = hasProperty("keeper.releaseMode")
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin-api:1.4.21")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin-api:1.4.30")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
 
   if (releaseMode) {
     compileOnly("com.android.tools.build:gradle:$defaultAgpVersion")
@@ -127,6 +127,6 @@ dependencies {
 
   testImplementation("com.squareup:javapoet:1.13.0")
   testImplementation("com.squareup:kotlinpoet:1.7.2")
-  testImplementation("com.google.truth:truth:1.1")
-  testImplementation("junit:junit:4.13.1")
+  testImplementation("com.google.truth:truth:1.1.2")
+  testImplementation("junit:junit:4.13.2")
 }
