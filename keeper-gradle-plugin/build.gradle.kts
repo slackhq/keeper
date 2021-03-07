5cf3ecac50f9ad932c7abd3ccea9b646a34ec014
Zac Sweers <pandanomic@gmail.com>
2021-03-07 16:10:51 -0500

    Make ZipSource instantiation compatible with multiple versions

--- keeper-gradle-plugin/build.gradle.kts
+++ keeper-gradle-plugin/build.gradle.kts
@@ -96,7 +96,7 @@ tasks.named<DokkaTask>("dokkaHtml") {
   }
 }
 
-val defaultAgpVersion = "4.0.0"
+val defaultAgpVersion = "4.1.2"
 val agpVersion = findProperty("keeperTest.agpVersion")?.toString() ?: defaultAgpVersion
 
 // See https://github.com/slackhq/keeper/pull/11#issuecomment-579544375 for context
--- keeper-gradle-plugin/src/main/java/com/slack/keeper/ZipFlingerExt.kt
+++ keeper-gradle-plugin/src/main/java/com/slack/keeper/ZipFlingerExt.kt
@@ -3,6 +3,7 @@ package com.slack.keeper
 import com.android.zipflinger.ZipArchive
 import com.android.zipflinger.ZipSource
 import java.io.File
+import java.nio.file.Path
 
 /**
  * Returns a sequence of pairs representing the class files and their relative names for use in a
@@ -20,7 +21,7 @@ internal fun File.classesSequence(): Sequence<Pair<String, File>> {
  * Extracts classes from the target [jar] into this archive.
  */
 internal fun ZipArchive.extractClassesFrom(jar: File, callback: (String) -> Unit) {
-  val jarSource = ZipSource(jar)
+  val jarSource = newZipSource(jar)
   jarSource.entries()
       .filterNot { "META-INF" in it.key }
       .forEach { (name, entry) ->
@@ -32,4 +33,18 @@ internal fun ZipArchive.extractClassesFrom(jar: File, callback: (String) -> Unit
         }
       }
   add(jarSource)
+}
+
+private fun newZipSource(jar: File): ZipSource {
+  return try {
+    // AGP 4.1/4.2
+    ZipSource::class.java
+        .getDeclaredConstructor(File::class.java)
+        .newInstance(jar)
+  } catch (e: NoSuchMethodException) {
+    // AGP/ZipFlinger 7+
+    ZipSource::class.java
+        .getDeclaredConstructor(Path::class.java)
+        .newInstance(jar.toPath())
+  }
 }
