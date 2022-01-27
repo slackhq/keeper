8cb608d023192b3193c64882775ea28a45acbceb
Zac Sweers <pandanomic@gmail.com>
2022-01-27 16:25:06 -0500

    Disable config cache for website deployments

--- deploy_website.sh
+++ deploy_website.sh
@@ -24,7 +24,7 @@ if ! [[ ${local} ]]; then
   cd ${DIR}
 
   # Generate the API docs
-  ./gradlew -p keeper-gradle-plugin dokkaGfm
+  ./gradlew -p keeper-gradle-plugin dokkaGfm --no-configuration-cache
 
 fi
 
