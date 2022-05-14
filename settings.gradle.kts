224e91700e7324c87ab56789a7f04eace601d71e
Zac Sweers <pandanomic@gmail.com>
2022-05-13 22:59:21 -0400

    Fix dependencyresolutionmanagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+dependencyResolutionManagement {
+  versionCatalogs {
+    if (System.getenv("DEP_OVERRIDES") == "true") {
+      val overrides = System.getenv().filterKeys { it.startsWith("DEP_OVERRIDE_") }
+      maybeCreate("libs").apply {
+        for ((key, value) in overrides) {
+          val catalogKey = key.removePrefix("DEP_OVERRIDE_").toLowerCase()
+          println("Overriding $catalogKey with $value")
+          version(catalogKey, value)
+        }
+      }
+    }
+  }
+}
+
 rootProject.name = "keeper-root"
 include(":sample")
 include(":sample-libraries:a")
