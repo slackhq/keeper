f85328733627b847300bab37ca090e9eb90fce9b
Zac Sweers <pandanomic@gmail.com>
2022-12-06 16:18:08 -0500

    Modernize CI and publishing

--- .github/workflows/run_instrumentation_tests.sh
+++ .github/workflows/run_instrumentation_tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-AGP_VERSION=$1
-ENABLE_TRACEREFS=$2
-
-# We only run the sample with R8 as proguard infinite loops if we have java 8 libraries on the classpath 🙃
 echo "Building APK and verifying L8"
-./gradlew :sample:minifyExternalStagingWithR8 --stacktrace -PkeeperTest.agpVersion="${AGP_VERSION}" -PkeeperTest.enableTraceReferences="${ENABLE_TRACEREFS}" -Pkeeper.verifyL8=true
+./gradlew :sample:minifyExternalStagingWithR8 --stacktrace -Pkeeper.verifyL8=true
+
 # Reclaim memory because Actions OOMs sometimes with having both an emulator and heavy gradle builds going on
 ./gradlew --stop || jps|grep -E 'KotlinCompileDaemon|GradleDaemon'| awk '{print $1}'| xargs kill -9 || true
+
 # Now proceed, with much of the build being cached up to this point
 echo "Running instrumentation tests"
-./gradlew connectedExternalStagingAndroidTest --stacktrace -PkeeperTest.agpVersion="${AGP_VERSION}" -PkeeperTest.enableTraceReferences="${ENABLE_TRACEREFS}"
+./gradlew connectedExternalStagingAndroidTest --stacktrace
--- publish.sh
+++ publish.sh
@@ -4,10 +4,10 @@ if [[ "$1" = "--snapshot" ]]; then snapshot=true; fi
 if [[ "$1" = "--local" ]]; then local=true; fi
 
 if ! [[ ${local} ]]; then
-  ./gradlew -p keeper-gradle-plugin clean publish --no-daemon --no-parallel --no-configuration-cache -Pkeeper.releaseMode=true
+  ./gradlew -p keeper-gradle-plugin publish
   if ! [[ ${snapshot} ]]; then
     ./gradlew -p keeper-gradle-plugin closeAndReleaseRepository
   fi
 else
-  ./gradlew -p keeper-gradle-plugin clean install --no-daemon --no-parallel --no-configuration-cache -Pkeeper.releaseMode=true
+  ./gradlew -p keeper-gradle-plugin publishToMavenLocal
 fi
