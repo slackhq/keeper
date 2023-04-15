144a4490526e6beedc013e44e7fb3d149c7d5762
Zac Sweers <pandanomic@gmail.com>
2023-04-15 23:57:57 +0200

    Prepare next development version.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 if [[ "$1" = "--local" ]]; then local=true; fi
 
 if ! [[ ${local} ]]; then
-  ./gradlew -p keeper-gradle-plugin publish
+  ./gradlew -p keeper-gradle-plugin publish -x dokkaHtml
 else
-  ./gradlew -p keeper-gradle-plugin publishToMavenLocal
+  ./gradlew -p keeper-gradle-plugin publishToMavenLocal -x dokkaHtml
 fi
